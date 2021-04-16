7267373b508968811025f94bce81b308e977e953
aurore.bussalb <aurore.bussalb@mensiatech.com>
2021-04-16 10:39:57 +0200

    make this app compatible with BL and add all parameters

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-# app-create-events
+# app-get-events
+
+This is a draft of a future Brainlife App using MNE Python to extract existing events ([`mne.find_events`](https://mne.tools/stable/generated/mne.find_events.html?highlight=find_events#mne.find_events) or create events ([`mne.make_fixed_length_events`](https://mne.tools/stable/generated/mne.make_fixed_length_events.html?highlight=make_fixed_length_events#mne.make_fixed_length_events).
+
+# app-get-events documentation
+
+1) Extract or create existing events and store them in a .tsv file. 
+2) This file can be used to make epochs or when the data is resampled.
+3) Input files are:
+    * a MEG file in `.fif` format,
+    * an optional fine calibration file in `.dat`,
+    * an optional crosstalk compensation file in `.fif`,
+    * an optional head position file in `.pos`,
+    * an optional destination file in `.fif`,
+4) Input parameters are:
+    * `param_make_events`: `bool`, if True, fixed length events will be created, else existing events from the fif file will be extracted.
+    * `param_make_events_id`: `int`, the id to use. Default is 1.
+    * `param_make_events_start`: `float`, time of first event. Default is 0.
+    * `param_make_events_stop`: `float`, optional,  maximum time of last event. If None, events extend to the end of the recording. Default is None.
+    * `param_make_events_duration`: `float`, the duration in seconds to separate events by. Default is 1.
+    * `param_make_events_first_samp`: `str`, if True, times will have raw.first_samp added to them. Default is True.
+    * `param_make_events_overlap`: `float`, the overlap between events. Must be 0 <= overlap < duration. Default is 0.
+    * `param_find_events_stim_channels`: `str`, `list of str`, optional, name of the stim channel or all the stim channels affected by triggers. Default is None.
+    * `param_find_events_output`: `str`, whether to report when events start, when events end, or both. Either 'onset', 'offset', or 'step'. Default is 'onset'.
+    * `param_find_events_consecutive`: `bool` or `str`, if True, consider instances where the value of the events channel changes without first returning 
+        to zero as multiple events. If False, report only instances where the value of the events channel 
+        changes from/to zero. If ‘increasing’, report adjacent events only when the second event code is greater than the first. Default is 'increasing'.
+    * `param_find_events_min_duration`: `float`, the minimum duration of a change in the events channel required to consider it as an event (in seconds).
+        Default is 0.
+    * `param_find_events_shortest_event`, `int`, minimum number of samples an event must last. Default is 2.
+    * `param_find_events_mask`, `int`, optional, The value of the digital mask to apply to the stim channel values. If None, no masking is performed. Default is None.
+    * `param_find_events_uint_cast`, `bool`, if True, do a cast to uint16 on the channel data. Default is False.
+    * `param_find_events_mask_type`, `str`, The type of operation between the mask and the trigger. Either 'and' or 'not_and'. Default is 'and'.
+    * `param_find_events_initial_event`: `bool`, if True, an event is created if the stim channel has a value different from 0 as its first sample. Default is False.
+      
+This list along with the default values correspond to the parameters of MNE Python version 0.22.0.
+
+5) Ouput files are:
+    * a `.tsv` file containing the matrix of events.
+
+### Authors
+- [Aurore Bussalb](aurore.bussalb@icm-institute.org)
+
+### Contributors
+- [Aurore Bussalb](aurore.bussalb@icm-institute.org)
+- [Maximilien Chaumon](maximilien.chaumon@icm-institute.org)
+
+### Funding Acknowledgement
+brainlife.io is publicly funded and for the sustainability of the project it is helpful to Acknowledge the use of the platform. We kindly ask that you acknowledge the funding below in your code and publications. Copy and past the following lines into your repository when using this code.
+
+[![NSF-BCS-1734853](https://img.shields.io/badge/NSF_BCS-1734853-blue.svg)](https://nsf.gov/awardsearch/showAward?AWD_ID=1734853)
+[![NSF-BCS-1636893](https://img.shields.io/badge/NSF_BCS-1636893-blue.svg)](https://nsf.gov/awardsearch/showAward?AWD_ID=1636893)
+[![NSF-ACI-1916518](https://img.shields.io/badge/NSF_ACI-1916518-blue.svg)](https://nsf.gov/awardsearch/showAward?AWD_ID=1916518)
+[![NSF-IIS-1912270](https://img.shields.io/badge/NSF_IIS-1912270-blue.svg)](https://nsf.gov/awardsearch/showAward?AWD_ID=1912270)
+[![NIH-NIBIB-R01EB029272](https://img.shields.io/badge/NIH_NIBIB-R01EB029272-green.svg)](https://grantome.com/grant/NIH/R01-EB029272-01)
+
+### Citations
+1. Avesani, P., McPherson, B., Hayashi, S. et al. The open diffusion data derivatives, brain data upcycling via integrated publishing of derivatives and reproducible open cloud services. Sci Data 6, 69 (2019). [https://doi.org/10.1038/s41597-019-0073-y](https://doi.org/10.1038/s41597-019-0073-y)
+2. Taulu S. and Kajola M. Presentation of electromagnetic multichannel data: The signal space separation method. Journal of Applied Physics, 97 (2005). [https://doi.org/10.1063/1.1935742](https://doi.org/10.1063/1.1935742)
+3. Taulu S. and Simola J. Spatiotemporal signal space separation method for rejecting nearby interference in MEG measurements. Physics in Medicine and Biology, 51 (2006). [https://doi.org/10.1088/0031-9155/51/7/008](https://doi.org/10.1088/0031-9155/51/7/008)
+
+
+## Running the App 
+
+### On Brainlife.io
+
+This App is still private.
+
+### Running Locally (on your machine)
+
+1. git clone this repo
+2. Inside the cloned directory, create `config.json` with the same keys as in `config.json.example` but with paths to your input 
+   files and values of the input parameters. For instance:
+
+```json
+{
+  "fif": "rest1-raw.fif"
+}
+```
+
+3. Launch the App by executing `main`
+
+```bash
+./main
+```
+
+## Output
+
+The output file is .tsv file.
---
+++ get_events.py
@@ -0,0 +1,159 @@
+#!/usr/local/bin/python3
+
+import json
+import mne/
+import numpy as np
+import warnings
+import os
+import shutil
+
+def get_events(raw, param_make_events):
+    """Extract existing events from a fif file or create new events.
+
+    Parameters
+    ----------
+    raw: instance of mne.io.Raw
+        Data from which the events will be extracted or created.
+    param_make_events: bool
+        If True, fixed length events will be created, else existing events from the fif file
+        will be extracted.
+    param_make_events_id: int
+        The id to use. Default is 1.
+    param_make_events_start: float
+        Time of first event. Default is 0.
+    param_make_events_stop: float or None
+        Maximum time of last event. If None (default), events extend to the end of the recording.
+    param_make_events_duration: float
+        The duration in seconds to separate events by. Default is 1.
+    param_make_events_first_samp: bool
+        If True (default), times will have raw.first_samp added to them.
+    param_make_events_overlap: float
+        The overlap between events. Must be 0 <= overlap < duration.
+        Default is 0.
+    param_find_events_stim_channels: None or str or list of str
+        Name of the stim channel or all the stim channels affected by triggers.
+        If None (default), the config variables ‘MNE_STIM_CHANNEL’, ‘MNE_STIM_CHANNEL_1’, ‘MNE_STIM_CHANNEL_2’, etc. 
+        are read. If these are not found, it will fall back to ‘STI 014’ if present, then fall back to the 
+        first channel of type ‘stim’, if present. If multiple channels are provided then the returned events 
+        are the union of all the events extracted from individual stim channels.
+    param_find_events_output: str
+        Whether to report when events start, when events end, or both.
+        Either 'onset' (default), 'offset', or 'step'.
+    param_find_events_consecutive: bool or str
+        If True, consider instances where the value of the events channel changes without first returning 
+        to zero as multiple events. If False, report only instances where the value of the events channel 
+        changes from/to zero. If ‘increasing’ (default), report adjacent events only when the second event code 
+        is greater than the first.
+    param_find_events_min_duration: float
+        The minimum duration of a change in the events channel required to consider it as an event (in seconds).
+        Default is 0.
+    param_find_events_shortest_event: int
+        Minimum number of samples an event must last (default is 2). If the duration is less than 
+        this an exception will be raised.
+    param_find_events_mask: int or None
+        The value of the digital mask to apply to the stim channel values. If None (default), no masking is performed.
+    param_find_events_uint_cast: bool
+        If True (default False), do a cast to uint16 on the channel data.
+    param_find_events_mask_type: str
+        The type of operation between the mask and the trigger. Either 'and' (default) or 'not_and'.
+    param_find_events_initial_event: bool
+        If True (default False), an event is created if the stim channel has a value different from 0 as its first sample.
+
+    Returns
+    -------
+    events: .tsv file
+        File containing the matrix of events (shape (n_events, 3)).
+    """
+
+    # Create fixed length events
+    if param_make_events is True:
+        events = mne.make_fixed_length_events(raw, duration=10)
+
+    # Get events from raw file 
+    else:
+        events = mne.find_events(raw)
+
+    # Save events matrix
+    np.savetxt("out_dir_get_events/events.tsv", events, delimiter="\t")
+
+    return events
+
+
+def main():
+
+    # Generate a json.product to display messages on Brainlife UI
+    dict_json_product = {'brainlife': []}
+
+    # Load inputs from config.json
+    with open('config.json') as config_json:
+        config = json.load(config_json)
+
+    # Read the MEG file and save it in out_dir_get_events
+    data_file = config.pop('fif')
+    raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
+    raw.save("out_dir_get_events/meg.fif", overwrite=True)
+
+    # Read the crosstalk file
+    cross_talk_file = config.pop('crosstalk')
+    if os.path.exists(cross_talk_file) is True:
+        shutil.copy2(cross_talk_file, 'out_dir_get_events/crosstalk_meg.fif')  # required to run a pipeline on BL
+
+    # Read the calibration file
+    calibration_file = config.pop('calibration')
+    if os.path.exists(calibration_file) is True:
+        shutil.copy2(calibration_file, 'out_dir_get_events/calibration_meg.dat')  # required to run a pipeline on BL
+
+    # Read destination file 
+    destination_file = config.pop('destination')
+    if os.path.exists(destination_file) is True:
+        shutil.copy2(destination_file, 'out_dir_get_events/destination.fif')  # required to run a pipeline on BL
+
+    # Read head pos file
+    head_pos = config.pop('headshape')
+    if os.path.exists(head_pos) is True:
+        shutil.copy2(head_pos, 'out_dir_get_events/headshape.pos')  # required to run a pipeline on BL
+
+    # Check for None parameters
+
+    # make events stop
+    if config['param_make_events_stop'] == "":
+        config['param_make_events_stop'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
+
+    # stim channels
+    if config['param_find_events_stim_channels'] == "":
+        config['param_find_events_stim_channels'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
+
+    # mask
+    if config['param_find_events_mask'] == "":
+        config['param_find_events_mask'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
+
+    # Test if the data contains events
+    if raw.info['events'] is True and config['param_make_events'] is True:
+        user_warning_message = f'Events already exist in this raw file. ' \
+                               f'You are going to create a matrix of events ' \
+                               f'different from those contained in the raw file.'
+        warnings.warn(user_warning_message)
+        dict_json_product['brainlife'].append({'type': 'warning', 'msg': user_warning_message})
+
+    # Define kwargs
+    # Delete keys values in config.json when this app is executed on Brainlife
+    if '_app' and '_tid' and '_inputs' and '_outputs' in config.keys():
+        del config['_app'], config['_tid'], config['_inputs'], config['_outputs'] 
+    kwargs = config  
+    
+    # Create events
+    get_events(raw, **kwargs)
+
+    # Success message in product.json 
+    if config['param_make_events'] is True:   
+        dict_json_product['brainlife'].append({'type': 'success', 'msg': 'Events were successfully created.'})
+    else:
+        dict_json_product['brainlife'].append({'type': 'success', 'msg': 'Events were successfully extracted.'})
+
+    # Save the dict_json_product in a json file
+    with open('product.json', 'w') as outfile:
+        json.dump(dict_json_product, outfile)
+
+
+if __name__ == '__main__':
+    main()

85ca792fcf59fcb2b3e1ed660a176563f23c528b
AuroreBussalb <aurore.bussalb@gmail.com>
2021-05-21 14:39:24 +0200

    raw instead of data

--- get_events.py
+++ get_events.py
@@ -141,17 +141,17 @@ def main():
             # Select bad channels' name
             bad_channels = df_channels[df_channels["status"] == "bad"]['name']
             bad_channels = list(bad_channels.values)
-            # Put channels.tsv bad channels in data.info['bads']
-            data.info['bads'].sort() 
+            # Put channels.tsv bad channels in raw.info['bads']
+            raw.info['bads'].sort() 
             bad_channels.sort()
             # Warning message
-            if data.info['bads'] != bad_channels:
+            if raw.info['bads'] != bad_channels:
                 user_warning_message_channels = f'Bad channels from the info of your data file are different from ' \
                                                 f'those in the channels.tsv file. By default, only bad channels from channels.tsv ' \
                                                 f'are considered as bad: the info of your data file is updated with those channels.'
                 warnings.warn(user_warning_message_channels)
                 dict_json_product['brainlife'].append({'type': 'warning', 'msg': user_warning_message_channels})
-                data.info['bads'] = bad_channels
+                raw.info['bads'] = bad_channels
 
 
     # Convert all "" into None when the App runs on BL
